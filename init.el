4ce43788f0a25c71714aed756196953c556610e4
Huayong Kuang <sudohuoty@163.com>
2021-07-07 23:52:42 +0800

    加了点基础配置以及安装了markdown插件

--- init.el
+++ init.el
@@ -1,19 +1,15 @@
 ;; Emacs Config File
-;; By huoty 2021.07.04
-;; Version 0.0.1
+;; By Huoty 2021.07.04
+;; Version 0.0.2
 
 ;; 删除顶部 Bar
 (menu-bar-mode -1)
-(tool-bar-mode -1)
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
+;(tool-bar-mode -1)
 
 ;; 显示行号
 
+;; 不生成备份文件
+(setq-default make-backup-files nil)
 
 ;; 编码设置
 (set-locale-environment "UTF-8")
@@ -24,3 +20,25 @@
 (setq default-buffer-file-coding-system 'utf-8)
 (prefer-coding-system 'chinese-gbk)
 (prefer-coding-system 'utf-8)
+
+;; 保存时删除行尾空格及文件尾空行
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; 包管理
+(package-initialize)
+(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+                         ("org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(markdown-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
