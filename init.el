1e5911c3769200170520259d8ec4043ea7aad0bb
Huayong Kuang <sudohuoty@163.com>
2021-07-07 13:33:40 +0800

    update init config

--- init.el
+++ init.el
@@ -3,7 +3,24 @@
 ;; Version 0.0.1
 
 ;; 删除顶部 Bar
+(menu-bar-mode -1)
 (tool-bar-mode -1)
 
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
 
 ;; 显示行号
+
+
+;; 编码设置
+(set-locale-environment "UTF-8")
+(set-language-environment 'UTF-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(prefer-coding-system 'chinese-gbk)
+(prefer-coding-system 'utf-8)
