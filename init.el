23464c9d321e1d07fb302563943ecda068fcc88c
Huayong Kuang <sudohuoty@163.com>
2021-07-14 23:12:55 +0800

    自动安装包以及添加以及基础配置

--- init.el
+++ init.el
@@ -2,11 +2,31 @@
 ;; By Huoty 2021.07.04
 ;; Version 0.0.2
 
+;; 个人信息
+(setq user-full-name "Huoty Kong")
+(setq user-mail-address "sudohuoty@163.com")
+
 ;; 删除顶部 Bar
 (menu-bar-mode -1)
 ;(tool-bar-mode -1)
 
+;; 隐藏启动显示画面
+(setq inhibit-startup-screen t)
+
+;; 用空格代替 Tab 来进行缩进
+(setq-default indent-tabs-mode nil)
+
 ;; 显示行号
+;(global-linum-mode t)
+;(setq linum-format "%4d ")
+
+;; 在行首 C-k 时，同时删除末尾换行符
+(setq kill-whole-line t)
+
+;; 记住上次文件打开的位置
+(require 'saveplace)
+(save-place-mode 1)
+(setq-default save-place t)
 
 ;; 不生成备份文件
 (setq-default make-backup-files nil)
@@ -25,17 +45,28 @@
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; 包管理
+(require 'package)
 (package-initialize)
 (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
                          ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
                          ("org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")))
+(setq my-package-list ;启动时自动安装的插件
+      '(
+        markdown-mode
+        ))
+(dolist (package my-package-list)
+  (unless (package-installed-p package)
+    (unless package-archive-contents
+      (package-refresh-contents))
+    (package-install package)))
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(markdown-mode)))
+ '(package-selected-packages (quote (markdown-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
